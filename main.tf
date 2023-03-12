28a456b3cdf9a8c6c0f9a5998218e19b3f0c4bfc
charliepoker <iheanachocharlie@gmail.com>
2023-03-12 08:51:03 -0400

    updated public subnet

--- main.tf
+++ main.tf
@@ -18,7 +18,11 @@ data "aws_availability_zones" "available" {
 }
 
 
-# Create public subnets
+###############################################
+###############################################
+############## PUBLIC SUBNET ##################
+###############################################
+###############################################
 resource "aws_subnet" "public" {
   count                   = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
   vpc_id                  = aws_vpc.main.id
@@ -34,6 +38,11 @@ resource "aws_subnet" "public" {
   )
 }
 
+###############################################
+###############################################
+############## PRIVATE SUBNET #################
+###############################################
+###############################################
 resource "aws_subnet" "private" {
   count                   = var.preferred_number_of_private_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_private_subnets
   vpc_id                  = aws_vpc.main.id
